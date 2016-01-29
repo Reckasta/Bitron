9853dc69cd07bcedfb8251f9d2d9a232719b5456
Mjone77 <117526@S009RMB123-28.ads.friscoisd.org>
2016-01-29 10:05:06 -0600

    Started comment parsing and replying - close to initial release :)

--- main.py
+++ main.py
@@ -20,37 +20,69 @@ try:
         refreshMessage = config['Welcome']['refresh']
         alreadyWelcomed = config['Welcome']['alreadyWelcomed'].split()
         admins = config['Admin']['users'].split(',')
+        alreadyReplied = config['Technical']['alreadyReplied'].split(',')
+        alreadyWelcomed = config['Technical']['alreadyWelcomed'].split(',')
     except:
         config = configparser.ConfigParser()
         config['Welcome'] = {'message': 'Welcome!',
                              'alreadyWelcomed': '',
                              'refresh': 'Refresh'}
         config['Admin'] = {'users': ''}
+        config['Technical'] = {'alreadyReplied': '',
+                               'alreadyWelcomed': ''}
         with open('config.ini','w') as configfile:
             config.write(configfile)
         print("Please complete generated config.ini")
         sys.exit()
     
-    '''
     #log's on to reddit here and set the subreddit
     r = praw.Reddit('who really cares what goes here???')
-    #r.login('FacilityAI', '<passwordhere>')
+    r.login('FacilityAI', '<passwordhere>')
     subreddit = r.get_subreddit('secretsubreddit')
-    '''
     message = "" #add replies to this. Make sure to add a \n___\n (3 underscores) after every message. Hopefully this will allow multiple commands to be called with one comment
     
+    #Add comment or post id's to array and config.ini
+    #idType: 0 is a comment.id, 1 is a username
+    def addID(idToAdd, idType):
+        if idType == 0:
+            alreadyReplied.append(idToAdd)
+            config['Technical']['alreadyReplied']+=(idToAdd+',')
+        elif idType == 1:
+            alreadyWelcomed.append(idToAdd)
+            config['Technical']['alreadyWelcomed']+=(idToAdd+',')
+        
     #Checks the comments/posts/pm's for commands, and handles them
     def checkForCommands():
         print("checkForCommands")
+        global message
+        recentComments = r.get_comments('secretsubreddit')
+        for comment in recentComments:
+            if comment.body().contains('!') and str(comment.id) not in alreadyReplied:
+                addID(str(comment.id), 0)
+                message = ''
+                commentWords = comment.body().lower().split(' ')
+                if '!roll' in commentWords:
+                    try:
+                        numbers = commentWords[commentWords.index('!roll')+1].split('d')
+                        rollDice(numbers[0], numbers[1])
+                    except:
+                        message+='[META]To roll dice, please use !roll followed by #d#, for example saying \"!roll 3d5\" would roll 3 dice with 5 sides each.\n___\n'
+                if '!cake' in commentWords:
+                    cake()
+                if '!refresh' in commentWords:
+                    refreshMemory()
+                if message:
+                    commentReply(comment)
+        for submission in subreddit.get_new(limit=3):
+            if submission poster not in alreadyWelcomed: #how find submission OP?
+                message = ''
+                welcome(submission)
+            
         
     #Reply to comments
-    def commentReply(): #Don't forget takes in comment
+    def commentReply(comment): #Don't forget takes in comment
         global message
-        print("commentReply")
-        print(message+"^^[meta][^^See ^^all ^^my ^^commands](wikilinkhere.com)^^|[suggest ^^new ^^features](linkToPmBot.com)^^|other ^^info ^^here")
-        '''
         comment.reply(message+"^^[meta][^^See ^^all ^^my ^^commands](wikilinkhere.com)^^|[suggest ^^new ^^features](linkToPmBot.com)^^|other ^^info ^^here")
-        '''
         #is there a way to do the ^^ effect without having to do it before every word?
         
     #rolls dice and saves each roll in an array, total of the rolls is at the end of the array. This is done so that a table of the rolls can be made if needed.
@@ -77,10 +109,11 @@ try:
     
     #this is the welcome message. Will be replied to posts of users that have never posted before
     #ready for initial usage in the bot - can be upgraded later
-    def welcome():
+    def welcome(submission):
         global message
-        message+=welcomeMessage+'\n'
+        message+=welcomeMessage+"\n"
         refreshMemory()
+        submission.add_comment(message+"^^[meta][^^See ^^all ^^my ^^commands](wikilinkhere.com)^^|[suggest ^^new ^^features](linkToPmBot.com)^^|other ^^info ^^here")
         
     #this is the second part of the welcome message that people can ask for on demand
     #ready for initial usage in the bot - can be upgraded later
